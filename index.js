608fa07ccb82e541e9bebae5d60cf3b8869d8a4b
rhgoda <135763987+piliya-t-s@users.noreply.github.com>
2023-11-26 09:53:04 +0300

    cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import 'dotenv/config'
 import grpc from '@grpc/grpc-js'
 import protoLoader from '@grpc/proto-loader'
-
+import cors from 'cors'
 
 const packageDef = protoLoader.loadSync("api.proto", {})
 const hostPackage = grpc.loadPackageDefinition(packageDef).hostApi
@@ -10,6 +10,7 @@ const hostPackage = grpc.loadPackageDefinition(packageDef).hostApi
 const client = new hostPackage.Host(process.env.HOST, grpc.credentials.createInsecure())
 
 const app = express()
+app.use(cors())
 
 app.post('/room/', function(req, res) {
     let id = Date.now().toString().slice(-5)
